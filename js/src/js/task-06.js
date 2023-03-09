92025a05c7c19ba65ee45058346ffea6b147986a
NataliaValchuk <trewax321@gmail.com>
2023-03-09 16:26:55 +0200

    Update task-06.js

--- js/src/js/task-06.js
+++ js/src/js/task-06.js
@@ -39,16 +39,21 @@
 // });
 
 
-const input = document.querySelector('#validation-input');
+// const input = document.querySelector('#validation-input');
+// const dataLength = input.getAttribute('data-length');
+
+// input.addEventListener('blur', () => {
+//   const inputValue = input.value;
+//   if (inputValue.length === Number(dataLength)) {
+//     input.classList.add('valid');
+//     input.classList.remove('invalid');
+//   } else {
+//     input.classList.add('invalid');
+//     input.classList.remove('valid');
+//   }
+// });
+
+
+const input = document.querySelector('#validation - input');
 const dataLength = input.getAttribute('data-length');
 
-input.addEventListener('blur', () => {
-  const inputValue = input.value;
-  if (inputValue.length === Number(dataLength)) {
-    input.classList.add('valid');
-    input.classList.remove('invalid');
-  } else {
-    input.classList.add('invalid');
-    input.classList.remove('valid');
-  }
-});
