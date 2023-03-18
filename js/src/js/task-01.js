753e8a0dece1d60d748f7c7c8eda7f9a9f825508
NataliaValchuk <trewax321@gmail.com>
2023-03-18 19:37:04 +0200

    added task

--- js/src/js/task-01.js
+++ js/src/js/task-01.js
@@ -1,11 +1,20 @@
-const listWithClass = document.querySelectorAll('.item');
-console.log(`Number of categories: ${listWithClass.length}`);
-listWithClass.forEach(listEl => {
-  console.log('');
-  console.log(`Category: ${listEl.firstElementChild.textContent}`);
-  console.log(
-    `Elements: ${listEl.lastElementChild.querySelectorAll('li').length}`,
-  );
-});
-
-
+// const listWithClass = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${listWithClass.length}`);
+// listWithClass.forEach(listEl => {
+//   console.log('');
+//   console.log(`Category: ${listEl.firstElementChild.textContent}`);
+//   console.log(
+//     `Elements: ${listEl.lastElementChild.querySelectorAll('li').length}`,
+//   );
+// });
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '24px';
+console.log(listWithId);
+const listWithClass = document.querySelector('.menu');
+console.log(listWithClass);
+const menuItemsByClass = document.querySelectorAll('.menu-item');
+console.log(menuItemsByClass);
+const secondMenuItem = document.querySelector('.menu-item');
+secondMenuItem.style.color = 'tomato';
+console.log(secondMenuItem);
--- js/src/js/task-10.js
+++ js/src/js/task-10.js
@@ -1,3 +1,41 @@
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+// Функція для генерації випадкового кольору
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
+
+// Функція для створення колекції елементів
+function createBoxes(amount) {
+  // Створюємо масив з вказаною кількістю елементів
+  const boxes = new Array(amount).fill('');
+  // Генеруємо HTML-код для кожного елементу та об'єднуємо їх у рядок
+  const boxesMarkup = boxes.reduce((acc, _, index) => {
+    const size = 30 + index * 10;
+    const color = getRandomHexColor();
+    return (
+      acc +
+      `<div style="width: ${size}px; height: ${size}px; background-color: ${color}"></div>`
+    );
+  }, '');
+  // Додаємо HTML-код у контейнер для елементів
+  boxesContainer.insertAdjacentHTML('beforeend', boxesMarkup);
+}
+
+// Функція для очищення колекції елементів
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = document.querySelector('input').value;
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
